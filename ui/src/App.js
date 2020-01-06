c0f1719cd96ade5437b98e46002b643116e7266a
Andrew Allison <andrew.allison@zyrofiser.co.uk>
2020-01-06 16:58:32 +0000

    INITIAL COMMIT

---
+++ ui/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+import axios from 'axios';
+import logo from './logo.svg';
+
+const apiUrl = `http://localhost:8080`;
+
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  async createUser() {
+    await axios.get(apiUrl + '/user-create');
+    this.loadUsers();
+  }
+
+  async loadUsers() {
+    const res = await axios.get(apiUrl + '/users');
+    console.log(res);
+    this.setState({
+      users: res.data
+    });
+  }
+
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => this.createUser()}>Create User</button>
+          <p>Users list:</p>
+          <ul>
+            {this.state.users.map(user => (
+              <li key={user._id}>id: {user._id}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
